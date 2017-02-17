7eba93410adbbece2336e28d45d7661e68c49baf
Jorge Hernandez Garcia <jorgehdzg1991@gmail.com>
2017-02-17 11:43:14 -0600

    Limpieza de configuración y servicios innecesarios

--- app/config/services.php
+++ app/config/services.php
@@ -4,7 +4,6 @@ use Phalcon\Mvc\View;
 use Phalcon\Mvc\View\Engine\Php as PhpEngine;
 use Phalcon\Mvc\Url as UrlResolver;
 use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
-use Phalcon\Mvc\Model\Metadata\Memory as MetaDataAdapter;
 use Phalcon\Session\Adapter\Files as SessionAdapter;
 use Phalcon\Flash\Direct as Flash;
 
@@ -55,38 +54,6 @@ $di->setShared('view', function () {
     ]);
 
     return $view;
-});
-
-/**
- * Database connection is created based in the parameters defined in the configuration file
- */
-$di->setShared('db', function () {
-    $config = $this->getConfig();
-
-    $class = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
-    $params = [
-        'host'     => $config->database->host,
-        'username' => $config->database->username,
-        'password' => $config->database->password,
-        'dbname'   => $config->database->dbname,
-        'charset'  => $config->database->charset
-    ];
-
-    if ($config->database->adapter == 'Postgresql') {
-        unset($params['charset']);
-    }
-
-    $connection = new $class($params);
-
-    return $connection;
-});
-
-
-/**
- * If the configuration specify the use of metadata adapter use it or use memory otherwise
- */
-$di->setShared('modelsMetadata', function () {
-    return new MetaDataAdapter();
 });
 
 /**
