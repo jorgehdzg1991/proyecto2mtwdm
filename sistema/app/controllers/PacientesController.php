ce76e1157e32296e89284bc23bac26fc900a753d
Jorge Hernández García <jorgehdzg1991@gmail.com>
2017-03-03 13:08:26 -0600

    Vistas del modulo de pacientes terminadas

--- sistema/app/controllers/PacientesController.php
+++ sistema/app/controllers/PacientesController.php
@@ -12,21 +12,75 @@ class PacientesController extends SystemControllerBase
     {
         $this->moduleName = 'Mis pacientes';
         $this->tag->setTitle($this->moduleName);
+
+        $this->moduleCssLinks = [
+            'plugins/datatables/dataTables.css',
+            'plugins/form-select2/select2.css'
+        ];
+
+        $this->moduleJavaScripts = [
+            'plugins/datatables/jquery.dataTables.min.js',
+            'plugins/datatables/dataTables.bootstrap.js',
+            'plugins/form-select2/select2.min.js',
+            'js/modules/pacientes.js'
+        ];
+
         parent::initialize();
-    }
-
-    protected function defineModuleCssLinks()
-    {
-        parent::defineModuleCssLinks();
-    }
-
-    protected function defineModuleJavaScripts()
-    {
-        parent::defineModuleJavaScripts();
     }
 
     public function indexAction()
     {
+        $pacientes = [
+            [
+                'nombre' => 'Jorge',
+                'apellidoPaterno' => 'Hernández',
+                'apellidoMaterno' => 'García',
+                'sexo' => 'H',
+                'edad' => '25',
+                'peso' => '73 Kg',
+                'estatura' => '1.71 m',
+                'nacionalidad' => 'Mexicana',
+                'telefono' => '(477) 987 6543',
+                'correo' => 'jorge@test.com'
+            ],
+            [
+                'nombre' => 'Fulanita de Tal',
+                'apellidoPaterno' => 'Corrientín',
+                'apellidoMaterno' => 'López',
+                'sexo' => 'M',
+                'edad' => '21',
+                'peso' => '54 Kg',
+                'estatura' => '1.62 m',
+                'nacionalidad' => 'Mexicana',
+                'telefono' => '(477) 123 4567',
+                'correo' => 'fulanita@test.com'
+            ],
+            [
+                'nombre' => 'Juan',
+                'apellidoPaterno' => 'Cuerdas',
+                'apellidoMaterno' => 'Cuerdas',
+                'sexo' => 'H',
+                'edad' => '28',
+                'peso' => '80 Kg',
+                'estatura' => '1.55 m',
+                'nacionalidad' => 'Mexicana',
+                'telefono' => '(477) 159 7536',
+                'correo' => 'juan.cuerdas@test.com'
+            ]
+        ];
 
+        $this->view->pacientes = $pacientes;
+    }
+
+    public function nuevoAction()
+    {
+        $this->view->action = 'Nuevo paciente';
+
+        $sexo = [
+            'H' => 'Masculino',
+            'F' => 'Femenino'
+        ];
+
+        $this->view->sexo = $sexo;
     }
 }
---
+++ sistema/public/js/modules/pacientes.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.datatables').dataTable({
+        "sDom": "<'row'<'col-xs-6'l><'col-xs-6'f>r>t<'row'<'col-xs-6'i><'col-xs-6'p>>",
+        "sPaginationType": "bootstrap",
+        "oLanguage": {
+            "sLengthMenu": "Mostrando _MENU_ registros por página",
+            "sSearch": "",
+            "sInfo": "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
+            "sInfoEmpty": "",
+            "sEmptyTable": "No hay pacientes registrados",
+            "sInfoFiltered": "(filtrado de un total de _MAX_)",
+            "sZeroRecords": "No se encontraron registros que coincidan con los filtros especificados",
+            "oPaginate": {
+                "sPrevious": "Anterior",
+                "sNext": "Siguiente"
+            }
+        }
+    });
+    $('.dataTables_filter input').addClass('form-control').attr('placeholder','Buscar...');
+    $('.dataTables_length select').addClass('form-control');
+    $('#selSexo').select2();
+});
